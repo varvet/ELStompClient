b81adfd7b975f1d2b4d4853bb8f26764b1963282
Anders Carlsson <andersc@elabs.se>
2014-02-21 13:04:42 +0100

    Initial Commit

---
+++ ELStompClient/ELStompClient.h
@@ -0,0 +1,13 @@
+//
+//  ELStompClient.h
+//  ELStompClient
+//
+//  Created by Elabs Developer on 21/02/14.
+//  Copyright (c) 2014 Elabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ELStompClient : NSObject
+
+@end
---
+++ ELStompClient/ELStompClient.m
@@ -0,0 +1,13 @@
+//
+//  ELStompClient.m
+//  ELStompClient
+//
+//  Created by Elabs Developer on 21/02/14.
+//  Copyright (c) 2014 Elabs. All rights reserved.
+//
+
+#import "ELStompClient.h"
+
+@implementation ELStompClient
+
+@end
